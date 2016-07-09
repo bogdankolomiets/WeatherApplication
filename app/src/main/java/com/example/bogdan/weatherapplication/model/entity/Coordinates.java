242d24ad11f28585f39dc5a5c24e58e43705a2bf
Bogdan Kolomiets <kolom996@gmail.com>
2016-07-09 18:03:55 +0300

    #19 Refactoring. Adding Serialized names and Expose annotation

--- app/src/main/java/com/example/bogdan/weatherapplication/model/entity/Coordinates.java
+++ app/src/main/java/com/example/bogdan/weatherapplication/model/entity/Coordinates.java
@@ -1,19 +1,27 @@
 package com.example.bogdan.weatherapplication.model.entity;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 /**
  * @author Bogdan Kolomiets
  * @version 1
  * @date 06.07.16
  */
 public class Coordinates {
-  private double lat;
-  private double lon;
+  @SerializedName("lat")
+  @Expose
+  private double mLatitude;
+
+  @SerializedName("lon")
+  @Expose
+  private double mLongitude;
 
   public double getLat() {
-    return lat;
+    return mLatitude;
   }
 
   public double getLon() {
-    return lon;
+    return mLongitude;
   }
 }
